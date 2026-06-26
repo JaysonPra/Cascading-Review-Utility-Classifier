b174d388619683d7269fc7b7ac2dfe00c6235796
Jayson Pradhananga <74088170+JaysonPra@users.noreply.github.com>
2026-06-26 20:07:12 +0545

    review schema

---
+++ src/classifier_core/schemas/database.py
@@ -0,0 +1,8 @@
+from sqlmodel import Field, SQLModel
+
+
+class Review(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    content: str = Field(min_length=0)
+    score: int = Field(ge=1, le=5)
+    thumbsUpCount: int = Field(ge=0)
