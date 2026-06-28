ee18794a3d88a3040ef66868c451428ed4999c9e
Jayson Pradhananga <74088170+JaysonPra@users.noreply.github.com>
2026-06-28 18:29:42 +0545

    refactor: removed thumbsUpCount due to it being a bad predictor

--- src/classifier_core/schemas/database.py
+++ src/classifier_core/schemas/database.py
@@ -5,4 +5,3 @@ class Review(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     content: str = Field(min_length=0)
     score: int = Field(ge=1, le=5)
-    thumbsUpCount: int = Field(ge=0)
