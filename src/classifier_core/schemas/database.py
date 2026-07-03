ead4b68bbbcf3f150157db37059c9d7396095bc4
Jayson Pradhananga <74088170+JaysonPra@users.noreply.github.com>
2026-07-03 15:23:40 +0545

    feat: add label for review in the Review schema

--- src/classifier_core/schemas/database.py
+++ src/classifier_core/schemas/database.py
@@ -1,7 +1,10 @@
 from sqlmodel import Field, SQLModel
 
+from classifier_core.core.types import ReviewLabel
+
 
 class Review(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     content: str = Field(min_length=0)
     score: int = Field(ge=1, le=5)
+    label: ReviewLabel = Field(index=True)
