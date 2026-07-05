db19e9f7f203138d9046efcc8109f71a94aebf4d
Jayson Pradhananga <74088170+JaysonPra@users.noreply.github.com>
2026-07-05 18:40:23 +0545

    feat: set label to default to None

--- src/classifier_core/schemas/database.py
+++ src/classifier_core/schemas/database.py
@@ -1,10 +1,10 @@
 from sqlmodel import Field, SQLModel
 
-from classifier_core.core.types import ReviewLabel
+from classifier_core.core.types import ReviewLabelType
 
 
 class Review(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     content: str = Field(min_length=0)
     score: int = Field(ge=1, le=5)
-    label: ReviewLabel = Field(index=True)
+    label: ReviewLabelType | None = Field(index=True, default=None)
