178426be37c7946a669b32734d19fcc9628fdd30
Jayson Pradhananga <74088170+JaysonPra@users.noreply.github.com>
2026-07-07 19:22:17 +0545

    feat: add constant for config directory

--- src/classifier_core/core/constants.py
+++ src/classifier_core/core/constants.py
@@ -3,5 +3,7 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).resolve().parents[3]
 
 DATA_DIR = ROOT_DIR / "data"
-TOKEN_PATH = DATA_DIR / "continuation_token.pkl"
 LOGS_DIR = ROOT_DIR / "logs"
+CONFIG_DIR = ROOT_DIR / "configs"
+
+TOKEN_PATH = DATA_DIR / "continuation_token.pkl"
